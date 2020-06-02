d38059e9e394261d1871dcfe16df9084a53a9ad0
Andrew Kim <jeesoo2002@gmail.com>
2020-06-01 23:35:55 -0700

    setting layouts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Grid } from "@material-ui/core"
+import Header from "../comps/Header"
 // import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -10,8 +11,19 @@ const IndexPage = () => {
   return (
     <Layout>
       <Grid container direction="column">
-        <Grid item>This is Header</Grid>
-        <Grid item>This is Content</Grid>
+        <Grid item>
+          <Header />
+        </Grid>
+        <Grid item container display="flex">
+          <Grid item xs={0} sm={2}></Grid>
+          <Grid item xs={12} sm={8} justifyContent="center">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae
+            earum consequatur, a dolor fuga natus eligendi voluptatibus ut
+            itaque, totam tempora minus optio aliquid perspiciatis odio nulla.
+            Error, deserunt soluta?
+          </Grid>
+          <Grid xs={0} sm={2}></Grid>
+        </Grid>
       </Grid>
     </Layout>
   )
